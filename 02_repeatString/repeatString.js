313ae065910d010ff98d3478d5b223d826a6b9c8
EfteYo <58327339+EfteYo@users.noreply.github.com>
2023-02-22 15:43:33 +0100

    Pass exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    if (n < 0) {
+        return 'ERROR';
+    }
+    var output = '';
+    for(let i = 0; i < n; i++) {
+        output += word;
+    }
+    return output;
 };
 
 // Do not edit below this line
